5a20e44fc3fd27bfd8c53c5c56fa2b238e006c20
student-kiitcollege <jenasourav2001@gmail.com>
2025-06-28 00:06:36 +0530

    Updated Code

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   build: {
     outDir: 'dist',
   },
-  // Required for proper routing on Vercel
-  base: './',
+  base: './', 
 });
